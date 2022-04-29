2fd9394f59eb0cc6cff318031b672ac7fbb0f60a
sachin bapure <sachinbapure@sachins-MacBook-Pro.local>
2022-04-29 20:32:58 +0530

    nice, added tld : 42

--- contracts/Domains.sol
+++ contracts/Domains.sol
@@ -1,11 +1,61 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
+import { StringUtils } from "./libraries/StringUtils.sol";
 import  'hardhat/console.sol';
 
 contract Domains{
 
-    constructor() {
-        console.log("This is the domain Contract. Let's build the next generation naming service");
+    string public tld;
+
+    mapping (string => address) public domains;
+
+    mapping (string => string) public records;
+
+    constructor(string memory _tld) payable {
+        tld = _tld;
+        console.log("This is the universal TLD!");
+    }
+
+    function price (string memory name) public pure returns(uint) {
+        uint len = StringUtils.strlen(name);
+        require (len >0);
+        if (len == 3) {
+            return 5 * 10**17;
+        } else if (len == 4) {
+            return 3 * 10**17;
+        }
+        else {
+            return 1 * 10**17;
+        }
+        }
+    
+
+    function register(string calldata name) public payable{
+
+        require (domains[name] == address(0));
+
+        uint _price = price(name);
+
+        //Check if matic is paid in tranasction
+
+        require (msg.value >= _price, "Not enough fees");
+        
+        domains[name] = msg.sender;
+        
+
+    }
+
+    function getAddress (string calldata name) public view returns (address) {
+        return domains[name];
+    }
+
+    function setRecord(string calldata name, string calldata record) public {
+        require (domains[name] == msg.sender);
+        records[name] = record;
+    }
+
+    function getRecord(string calldata name) public view returns (string memory) {
+        return records[name];
     }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,9 +1,21 @@
 const main = async () => {
+    const [owner, randomPerson] = await hre.ethers.getSigners();
     const domainContractFactory = await hre.ethers.getContractFactory('Domains');
-    const domainContract = await domainContractFactory.deploy();
+    const domainContract = await domainContractFactory.deploy('42');
     await domainContract.deployed();
     console.log("contract deployed to:", domainContract.address);
-}
+    console.log("contract owner address:", owner.address);
+
+    const txn = await domainContract.register("mortal.pol",{value:hre.ethers.utils.parseEther('0.1')});
+    await txn.wait();
+
+    const domainOwner = await domainContract.getAddress("mortal.pol");
+    console.log(domainOwner);
+
+
+    const balance = await hre.ethers.provider.getBalance(domainContract.address);
+    console.log(hre.ethers.utils.formatEther(balance));
+}   
 
 const runMain = async () => {
     try {
